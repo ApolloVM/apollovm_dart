345d80112e24de2f2a97ff878cc5c0157b0a228b
gmpassos <gracilianomp@gmail.com>
2023-09-28 00:54:05 -0300

    Improve `apollovm_wasm_test.dart`

--- test/apollovm_wasm_test.dart
+++ test/apollovm_wasm_test.dart
@@ -10,18 +10,81 @@ void main() async {
           language: 'dart',
           code: r'''
       
-          int add(int a, int b) {
+          void empty() {
+            
+          }
+          
+        ''',
+          functionName: 'empty',
+          parameters: [],
+          expecteWasm: {
+            'test':
+                '0061736D010000000104016000000302010007090105656D70747900000A040102000B',
+          }),
+    );
+
+    test(
+      'basic 2',
+      () => _testWasm(
+          language: 'dart',
+          code: r'''
+      
+          int add1(int a) {
+            int x = a + 10;
+            return x;
+          }
+          
+        ''',
+          functionName: 'add1',
+          parameters: [101],
+          expectedResult: 111,
+          expecteWasm: {
+            'test':
+                '0061736D0100000001060160017F017F03020100070801046164643100000A0F010D01017F2000410A6A210120010B',
+          }),
+    );
+
+    test(
+      'basic 3',
+      () => _testWasm(
+          language: 'dart',
+          code: r'''
+      
+          int add3(int a, int b) {
             int x = a + b + 10;
             return x;
           }
           
         ''',
-          functionName: 'add',
+          functionName: 'add3',
           parameters: [101, 50],
           expectedResult: 161,
           expecteWasm: {
             'test':
-                '0061736D0100000001070160027F7F017F030201000707010361646400000A12011001017F20002001410A6A6A210220020B',
+                '0061736D0100000001070160027F7F017F03020100070801046164643300000A12011001017F20002001410A6A6A210220020B',
+          }),
+    );
+
+    test(
+      'basic 4',
+      () => _testWasm(
+          language: 'dart',
+          code: r'''
+      
+          int add4(int a, int b, int c) {
+            int x = a + b + 10;
+            int y = c ~/ 2;
+            int z = x - y;
+            return z;
+          }
+          
+        ''',
+          functionName: 'add4',
+          parameters: [101, 50, 30],
+          expectedResult: 146,
+          expecteWasm: {
+            'test':
+                '0061736D0100000001080160037F7F7F017F03020100070801046164643400000A24012203017F017F017F20002001410A6A6A210320024102A32104200320046B210520050B',
           }),
     );
   });
@@ -103,6 +166,21 @@ Future<void> _testWasm(
       print(wasmBytes);
 
       expect(wasmBytes, expectedBytes);
+
+      //_saveWasmFile(language, functionName, wasmBytes);
     }
   }
 }
+
+/*
+void _saveWasmFile(String language, String functionName, Uint8List wasmBytes) {
+  try {
+    var fileName = 'apollovm-$language-$functionName-test.wasm';
+
+    var file = File('/tmp/test-wasm/$fileName');
+    file.writeAsBytesSync(wasmBytes);
+
+    print('>> SAVED: $file');
+  } catch (_) {}
+}
+*/
